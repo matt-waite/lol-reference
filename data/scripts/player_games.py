5cf2a4be4b1ecbc681bf72147c36b376ba21b32a
Matt Waite <Matthew.Waite.SE@gmail.com>
2020-09-03 15:23:28 -0400

    Turn player game rows into mongo consumable object

--- data/scripts/player_games.py
+++ data/scripts/player_games.py
@@ -25,11 +25,42 @@ def get_players(filename):
                     players[player].append(reader.line_num)
     return players
 
-def get_player_games(name):
-    return []
+def get_player_games(file, name):
+    games = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if (row[cols['Player']] == name):
+                team = row[cols['Team']]
+                position = row[cols['Position']]
+                champion = row[cols['Champion']]
+                result = row[cols['Result']]
+                kills = row[cols['Kills']]
+                deaths = row[cols['Deaths']]
+                assists = row[cols['Assists']]
+                games.append({
+                    "player": name,
+                    "team": team,
+                    "position": position,
+                    "champion": champion,
+                    "result": False if result == "0" else True,
+                    "kills": int(kills),
+                    "deaths": int(deaths),
+                    "assists": int(assists)
+                })
+    return games
 
 if __name__ == "__main__":
     db = database.connect_db()
-    players = get_players("../event/EU_LCS_Summer_2014.csv")
-    for player in players.keys():
-        print(f"{player}: {len(players[player])}")
+    event = "../event/EU_LCS_Summer_2014.csv"
+    players = get_players(event)
+    for player in players:
+        games = get_player_games(event, player)
+        wins = 0
+        losses = 0
+        for game in games:
+            if game['result']:
+                wins += 1
+            else:
+                losses += 1
+        print(f"{player}: ({wins}-{losses})")
