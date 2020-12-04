ae02345148cdd82af688831846b2f90939dadb3a
Matt Waite <Matthew.Waite.SE@gmail.com>
2020-12-04 16:23:47 -0500

    display dummy list of players

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../player';
+import { PLAYERS } from '../mock-players';
 
 @Component({
   selector: 'app-players',
@@ -7,14 +8,15 @@ import { Player } from '../player';
   styleUrls: ['./players.component.css']
 })
 export class PlayersComponent implements OnInit {
-  player: Player = {
-    id: 1,
-    name: 'Bjergsen',
-    rating: 'Good'
-  };
+  players = PLAYERS;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedPlayer: Player;
+  onSelect(player: Player): void {
+    this.selectedPlayer = player;
   }
 }
